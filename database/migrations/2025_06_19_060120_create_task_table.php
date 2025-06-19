74e5c597f6059ac4eb2f5e024ccb770c901b3838
Edwin Isaac Avila Garcia <edwin.avilag1999@gmail.com>
2025-06-19 01:56:14 -0600

    fix: Cambio de nombre en la migracion

--- database/migrations/2025_06_19_060120_create_task_table.php
+++ database/migrations/2025_06_19_060120_create_task_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('task', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->string('description');
