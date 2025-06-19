33539c35ddb6c715a56cccb1d9ed2299165e0f41
Edwin Isaac Avila Garcia <edwin.avilag1999@gmail.com>
2025-06-19 01:20:32 -0600

    feat: Creación del repositorio Task

---
+++ app/Repositories/TaskRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Task;
+
+class TaskRepository {
+
+    public function findAll() {
+        return Task::all();
+    }
+
+    public function create(array $data) {
+        return Task::create($data);
+    }
+
+    public function update(Task $task, array $data) {
+        return $task->update($data);
+    }
+
+    public function delete(Task $task) {
+        return $task->delete();
+    }
+}
